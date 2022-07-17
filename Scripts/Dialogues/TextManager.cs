931f8eabfaa59f87146a408f733822cd90afeba9
Octavio Llanillo <llanillo4@hotmail.com>
2022-07-17 16:48:24 -0300

    Update TextManager.cs

--- Scripts/Dialogues/TextManager.cs
+++ Scripts/Dialogues/TextManager.cs
@@ -1,35 +1,37 @@
-using Godot;
 using System;
+using Godot;
 
-public class TextManager : Panel
+namespace JudgmentofLostSouls.Scripts.Dialogues
 {
-    private const float TextSpeedRate = 0.02f;
-    private const string Path = "Margin/VBox/";
+    public class TextManager : Panel
+    {
+        private const float TextSpeedRate = 0.02f;
+        private const string Path = "Margin/VBox/";
     
-    public PlayerState CurrentState { get; private set; }
+        public PlayerState CurrentState { get; private set; }
 
-    private AudioStreamPlayer _audioPlayerText;
-    private TextureButton _continueButton;
-    private Label _nameLabel;
-    private Label _textLabel;
-    private Tween _textTween;
+        private AudioStreamPlayer _audioPlayerText;
+        private TextureButton _continueButton;
+        private Label _nameLabel;
+        private Label _textLabel;
+        private Tween _textTween;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        CurrentState = PlayerState.READY;
-        _nameLabel = GetNode<Label>("../NamePanel/Margin/Name");
-        _audioPlayerText = GetNode<AudioStreamPlayer>(Path + "AudioPlayer");
-        _continueButton = GetNode<TextureButton>(Path + "ContinueButton");
-        _textLabel = GetNode<Label>(Path + "Text");
-        _textTween = GetNode<Tween>(Path + "TextTween");
-        _continueButton.Visible = false;
-    }
-
-    public override void _Input(InputEvent @event)
-    {
-        if (@event.IsActionPressed("ui_accept"))
+        // Called when the node enters the scene tree for the first time.
+        public override void _Ready()
         {
+            CurrentState = PlayerState.READY;
+            _nameLabel = GetNode<Label>("../NamePanel/Margin/Name");
+            _audioPlayerText = GetNode<AudioStreamPlayer>(Path + "AudioPlayer");
+            _continueButton = GetNode<TextureButton>(Path + "ContinueButton");
+            _textLabel = GetNode<Label>(Path + "Text");
+            _textTween = GetNode<Tween>(Path + "TextTween");
+            _continueButton.Visible = false;
+        }
+
+        public override void _Input(InputEvent @event)
+        {
+            if (!@event.IsActionPressed("ui_accept")) return;
+            
             switch (CurrentState)
             {
                 case PlayerState.READING:
@@ -38,45 +40,49 @@ public class TextManager : Panel
                 case PlayerState.FINISHED:
                     HandleFinishedState();
                     break;
+                case PlayerState.READY:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
         }
-    }
 
-    protected void HandleFinishedState()
-    {
-        _textLabel.Text = "";
-        _nameLabel.Text = "";
-        SwitchState(PlayerState.READY);
-    }
+        private void HandleFinishedState()
+        {
+            _textLabel.Text = "";
+            _nameLabel.Text = "";
+            SwitchState(PlayerState.READY);
+        }
 
-    public void OnTextTweenTweenCompleted(Godot.Object obj, NodePath node)
-    {
-        SwitchState(PlayerState.FINISHED);
-        _textLabel.PercentVisible = 1;
-        _continueButton.Visible = true;
-        _audioPlayerText.StreamPaused = true;
-        _textTween.RemoveAll();
-    }
+        private void OnTextTweenTweenCompleted(Godot.Object obj, NodePath node)
+        {
+            SwitchState(PlayerState.FINISHED);
+            _textLabel.PercentVisible = 1;
+            _continueButton.Visible = true;
+            _audioPlayerText.StreamPaused = true;
+            _textTween.RemoveAll();
+        }
 
-    public void DisplayText(string name, string text)
-    {
-        SwitchState(PlayerState.READING);
-        _audioPlayerText.StreamPaused = false;
-        _continueButton.Visible = false;
-        _nameLabel.Text = name;
-        _textLabel.Text = text;
-        _textLabel.PercentVisible = 0.0f;
-        _textTween.InterpolateProperty(_textLabel, "percent_visible", 0.0f, 1.0f, _textLabel.Text.Length * TextSpeedRate, Tween.TransitionType.Linear, Tween.EaseType.InOut);
-        _textTween.Start();
-    }
+        public void DisplayText(string name, string text)
+        {
+            SwitchState(PlayerState.READING);
+            _audioPlayerText.StreamPaused = false;
+            _continueButton.Visible = false;
+            _nameLabel.Text = name;
+            _textLabel.Text = text;
+            _textLabel.PercentVisible = 0.0f;
+            _textTween.InterpolateProperty(_textLabel, "percent_visible", 0.0f, 1.0f, _textLabel.Text.Length * TextSpeedRate, Tween.TransitionType.Linear, Tween.EaseType.InOut);
+            _textTween.Start();
+        }
 
-    public void OnContinueButtonPressed()
-    {
-        HandleFinishedState();
-    }
+        public void OnContinueButtonPressed()
+        {
+            HandleFinishedState();
+        }
 
-    protected void SwitchState(PlayerState nextState)
-    {
-        CurrentState = nextState;
+        private void SwitchState(PlayerState nextState)
+        {
+            CurrentState = nextState;
+        }
     }
 }

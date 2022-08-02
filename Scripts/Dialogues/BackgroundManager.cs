45473ee5fc29155fda51c4d1bad15b3b17048d68
Octavio Llanillo <llanillo4@hotmail.com>
2022-08-01 22:43:52 -0300

    Update BackgroundManager.cs

--- Scripts/Dialogues/BackgroundManager.cs
+++ Scripts/Dialogues/BackgroundManager.cs
@@ -1,39 +1,41 @@
 using Godot;
-using System;
 
-public class BackgroundManager : PanelContainer
+namespace JudgmentofLostSouls.Scripts.Dialogues
 {
-    [Export(PropertyHint.Dir)]
-    public string BackgroundPath;
-
-    private TextureRect _mainBackground;
-    private Sprite _secondaryBackground;
-    private AnimationPlayer _animationPlayerBackground;
-
-    private DialogueManager _dialogueManager;
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+    public class BackgroundManager : PanelContainer
     {
-        _dialogueManager = GetParent<Control>() as DialogueManager;
-        _mainBackground = GetNode<TextureRect>("MainBackground");
-        _secondaryBackground = GetNode<Sprite>("SecondaryBackground");
-        _animationPlayerBackground = GetNode<AnimationPlayer>("AnimationPlayer");
+        [Export(PropertyHint.Dir)]
+        public string BackgroundPath;
 
-        _dialogueManager.Connect("TransitionBackground", this, "AssignBackground");
-    }
+        private TextureRect _mainBackground;
+        private Sprite _secondaryBackground;
+        private AnimationPlayer _animationPlayerBackground;
 
-    private void AssignBackground(string background, bool playAnim)
-    {
-        if (playAnim)
-        {            
-            _animationPlayerBackground.Play("Transition");
-        }
-        else
+        private DialogueManager _dialogueManager;
+
+        // Called when the node enters the scene tree for the first time.
+        public override void _Ready()
         {
-            _secondaryBackground.Modulate = new Color(1, 1, 1, 0);
+            _dialogueManager = GetParent<Control>() as DialogueManager;
+            _mainBackground = GetNode<TextureRect>("MainBackground");
+            _secondaryBackground = GetNode<Sprite>("SecondaryBackground");
+            _animationPlayerBackground = GetNode<AnimationPlayer>("AnimationPlayer");
+
+            _dialogueManager.Connect("TransitionBackground", this, "AssignBackground");
         }
 
-        _mainBackground.Texture = (Texture) GD.Load(BackgroundPath + "/" + background + ".png");
+        private void AssignBackground(string background, bool playAnim)
+        {
+            if (playAnim)
+            {            
+                _animationPlayerBackground.Play("Transition");
+            }
+            else
+            {
+                _secondaryBackground.Modulate = new Color(1, 1, 1, 0);
+            }
+
+            _mainBackground.Texture = (Texture) GD.Load(BackgroundPath + "/" + background + ".png");
+        }
     }
 }
